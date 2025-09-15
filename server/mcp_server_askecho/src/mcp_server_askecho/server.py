2f720bfc4c249d814aaf4311e610eaf88461f74d
liqi0117 <liqi.0117@bytedance.com>
2025-09-15 11:40:09 +0800

    feat: add ask_echo mcp init params (#261)
    Change-Id: Icd2f493ed9fe9d088716471c552e3962d090cdf9

--- server/mcp_server_askecho/src/mcp_server_askecho/server.py
+++ server/mcp_server_askecho/src/mcp_server_askecho/server.py
@@ -17,7 +17,10 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 config = None
-mcp = FastMCP("AskEcho MCP Server")
+mcp = FastMCP("AskEcho MCP Server",
+              host=os.getenv("MCP_SERVER_HOST", "0.0.0.0"),
+              port=int(os.getenv("MCP_SERVER_PORT", "8000")),
+              streamable_http_path=os.getenv("STREAMABLE_HTTP_PATH", "/mcp"))
 
 
 @mcp.tool()
